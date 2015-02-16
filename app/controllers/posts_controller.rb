70a80e66f949d00a3d689f017c77a7743e73e8d4
benjamin saravia <elbenja@gmail.com>
2015-02-16 00:13:14 -0500

    validation added

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,15 +6,21 @@ class PostsController < ApplicationController
 	end
 
 	def new
+		@post = Post.new
 	end
 
 	def show
 	end
+
 	def create
 		@post = Post.new(post_params)
-		@post.save
+		if @post.save
 
-		redirect_to @post
+			redirect_to @post
+		else
+			render 'new'
+		end
+
 	end
 
 private
